7b9367967f6320f11d31bbba7c683db97ac70039
sirius.cheng <sirius.cheng@yunlsp.com>
2022-12-19 15:48:23 +0800

    feat: 新增组件

--- docs/.vuepress/components/OpenOutLink.vue
+++ docs/.vuepress/components/OpenOutLink.vue
@@ -1,15 +1,21 @@
 <template>
-  <div class="link-title-box" @click="handleClick" @mouseenter='handleToggleIcon' @mouseleave='handleToggleIcon'>
+  <span class="link-title-box" @click="handleClick" @mouseenter='handleToggleIcon' @mouseleave='handleToggleIcon'>
     <span >
       <slot></slot>
     </span>
     <HomeOutlined v-show='showIcon' class="icon" />
-  </div>
+  </span>
+  <Badge v-show="type" :type="type" :text="badgeText" vertical="top" class="badge" />
 </template>
 <script lang="ts">
 import { defineComponent, ref } from "vue";
 import { HomeOutlined } from "@ant-design/icons-vue";
 import { throttle } from 'lodash-es'
+enum BadgeTextEnum {
+  tip = '简单',
+  warning = '中等',
+  danger = '困难',
+}
 export default defineComponent({
   name: "ToggleBox",
   components: {
@@ -20,15 +26,22 @@ export default defineComponent({
       type: String,
       required: true,
     },
+    type: {
+      type: String,
+    },
+    vertical: {
+      type: String,
+    },
   },
   setup(props) {
     const showIcon = ref(false);
+    const badgeText = ref('');
+    
     const handleToggle = () => {
       showIcon.value = !showIcon.value;
     };
     const handleToggleIcon = throttle(
       () => {
-        console.log(222)
         handleToggle();
       },
       100
@@ -39,12 +52,18 @@ export default defineComponent({
         window.open(props.link);
       }
     };
+    const setBadgeText = () => {
+      console.log(2222)
+      badgeText.value = BadgeTextEnum[props.type];
+    }
+    setBadgeText();
       
     return {
       handleToggle,
       showIcon,
       handleToggleIcon,
       handleClick,
+      badgeText
     };
   },
 });
@@ -61,5 +80,8 @@ export default defineComponent({
   font-size: 20px;
   margin-left: 10px;
   color:var(--c-brand);
+}
+.badge{
+  margin-left: 10px;
 }
 </style>
